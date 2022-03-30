2168a89cc90f780328c30a65f4b08bc88a873731
Gerardo Miranda <gerardodev25@gmail.com>
2022-03-30 18:21:46 -0400

    build docs

---
+++ docs/static/js/main.27486c21.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpweb-personal"]=this["webpackJsonpweb-personal"]||[]).push([[0],{10:function(e,n,r){r.r(n);var t=r(1),c=r.n(t),o=r(3),s=r.n(o),a=(r(8),r(0));var i=function(){return Object(a.jsx)("div",{children:"Gerardo Miranda"})};s.a.render(Object(a.jsx)(c.a.StrictMode,{children:Object(a.jsx)(i,{})}),document.getElementById("root"))},8:function(e,n,r){}},[[10,1,2]]]);
+//# sourceMappingURL=main.27486c21.chunk.js.map
